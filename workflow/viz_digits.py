2858d5ca59b55efcf19c4887cce880cc5d2a4381
Cameron Kranz <cameronkranz2@gmail.com>
2026-03-03 14:25:45 -0500

    import data, extract data, and creat tSNE

--- workflow/viz_digits.py
+++ workflow/viz_digits.py
@@ -2,3 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
+data = pd.read_csv('../data/digits-data.csv')
+
+# Extract data
+pixel_values = data.iloc[:,:-1].values
+pixels = data.columns.tolist()
+pixels.pop()
+
+tSNE = TSNE(n_jobs=2,random_state=0)
