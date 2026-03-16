6a442397412ffcf515aa051e8c58c1926faed88f
Joel V Zachariah <joelvzach@gmail.com>
2026-03-15 21:58:55 -0400

    Add command to run format_1d_data.py

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 uv sync
 
+uv run python workflow/format_1d_data.py
 uv run python workflow/viz_1d_data.py
 uv run python workflow/viz_2d_data.py
 uv run python workflow/1d-multi-method-data-processing.py
